cd3fa992e8214cea1ad1ba2002148dc8d9a48d6d
--user.email <hacenedu92@live.fr>
2023-02-28 18:27:22 +0100

    beginning of user CRUD, setting up different menus depending on the role.

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,6 +11,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
 {
@@ -20,25 +21,12 @@ class RegistrationFormType extends AbstractType
             ->add('email', EmailType::class, [
                 "label" => "saisir  votre adresse mail"
             ])
-    
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+            ->add('plainPassword', RepeatedType::class, [
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-            ])
-        ;
+                'type' => PasswordType::class,
+                'first_options' => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmez le mot de passe'],
+            ]);;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
