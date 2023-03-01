794ff00cb367cf000a80176c141b88e36f090257
unknown <msekkai92@gmail.com>
2023-03-01 14:59:30 +0100

    contact form bug corrected

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,11 +1,8 @@
 <?php
 
 namespace App\Controller;
-use  App\Entity\Contact;
-use App\Form\ContactType;
-use App\Repository\ContactRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,24 +15,5 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig');
     }
-   /**
-     * @Route("/contact", name="contact", methods={"GET", "POST"})
-     */
-    public function contact(Request $request, ContactRepository $contactRepository): Response
-    {
-        $contact = new Contact();
-        $form = $this->createForm(ContactType::class, $contact);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $contactRepository->add($contact, true);
-
-            return $this->redirectToRoute('home', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('contact/new.html.twig', [
-            'contact' => $contact,
-            'form' => $form,
-        ]);
-    }
+  
 }
