dc53a689a2f2463360208b98851871c7bc435b56
--user.email <hacenedu92@live.fr>
2023-03-02 12:31:25 +0100

    corecteed bug in contact form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Contact;
+use App\Form\ContactType;
+use App\Repository\ContactRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
@@ -15,16 +19,35 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig');
     }
-  /**
+
+    /**
      * @Route("/about", name="app_about")
      */
     public function about(): Response
     {
         return $this->render(
             'home/about.html.twig',
-            [
-               
-            ]
+            []
         );
+    }
+
+    /**
+     * @Route("/contact", name="app_contact", methods={"GET", "POST"})
+     */
+    public function new(Request $request, ContactRepository $contactRepository): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactRepository->add($contact, true);
+
+            return $this->redirectToRoute('app_home', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('contact/new.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
