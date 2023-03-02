2e30f5d68f37dc651ed21bfebc7453bbf200a66c
--user.email <hacenedu92@live.fr>
2023-03-02 16:25:22 +0100

    adding crud for contact

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -1,29 +1,24 @@
 <?php
 
 namespace App\Controller;
-use app\Repository\ContactRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use App\Repository\ContactRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * @Route("/dashboard", name="dashboard", methods={"GET"})
+ */
 class DashboardController extends AbstractController
 {
     /**
-     * @Route("/dashboard", name="app_dashboard")
+     * @Route("/", name="dashboard_index")
      */
     public function index(): Response
     {
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
-        ]);
-    }
-     /**
-     * @Route("/dashboard/contact", name="dashobard_contact_index", methods={"GET"})
-     */
-    public function showContact(ContactRepository $contactRepository): Response
-    {
-        return $this->render('contact/index.html.twig', [
-            'contact' => $contactRepository->findAll(),
         ]);
     }
 }
