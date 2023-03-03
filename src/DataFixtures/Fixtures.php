71b27e768c54aeee6b7d109dfe647a67c2eb6e97
--user.email <hacenedu92@live.fr>
2023-03-03 13:07:03 +0100

    fix user fixtures that are functional.

--- src/DataFixtures/Fixtures.php
+++ src/DataFixtures/Fixtures.php
@@ -8,24 +8,32 @@ use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
-
 class Fixtures extends Fixture
 {
+    private $encoder;
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager): void
 
 
     {
-       
-       // creation des biens 
-        for($k=0; $k<20; $k++){
-            $user= New user();
-            $user->setName("nom".$k)
-                -> setEmail("email".$k."@gmail.com")
-                ->setPassword($this->encoder->encodePassword($user[$k], 'password'));
 
-        $manager->persist($user);
+        // creation des biens 
+        for ($k = 0; $k <= 20; $k++) {
+            $user = new User();
+            $user->setName("nom" . $k)
+                ->setEmail("email" . $k . "@gmail.com");
+            if ($k == 0) {
+                $user->setRoles(["ROLE_ADMIN"]);
+            } else {
+                $user->setRoles(["ROLE_USER"]);
+            }
+            $user->setPassword($this->encoder->encodePassword($user, 'password'));
+            $manager->persist($user);
         }
-
         $manager->flush();
     }
 }
