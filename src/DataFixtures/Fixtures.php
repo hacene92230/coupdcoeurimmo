08fe52e088f828ffffb9389a2eb971fd159edd54
unknown <msekkai92@gmail.com>
2023-03-03 12:30:10 +0100

    created fixture user

--- src/DataFixtures/Fixtures.php
+++ src/DataFixtures/Fixtures.php
@@ -2,15 +2,29 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
 
 class Fixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
+
+
     {
-        // $product = new Product();
-        // $manager->persist($product);
+       
+       // creation des biens 
+        for($k=0; $k<20; $k++){
+            $user= New user();
+            $user->setName("nom".$k)
+                -> setEmail("email".$k."@gmail.com")
+                ->setPassword($this->encoder->encodePassword($user[$k], 'password'));
+
+        $manager->persist($user);
+        }
 
         $manager->flush();
     }
