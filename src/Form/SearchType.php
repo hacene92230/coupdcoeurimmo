51c43939e83aa45e50e8eb58efe533b4af5212f4
--user.email <hacenedu92@live.fr>
2023-03-21 11:18:30 +0100

    form search

--- src/Form/SearchType.php
+++ src/Form/SearchType.php
@@ -3,12 +3,11 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class PropertySearchType extends AbstractType
+class SearchType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
@@ -27,7 +26,7 @@ class PropertySearchType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => false
+            'data_class' => null
 
         ]);
     }
