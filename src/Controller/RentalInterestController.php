8745fdb32045b160740d9a365b0aa9033693d1e3
unknown <msekkai92@gmail.com>
2023-05-31 11:48:36 +0200

    rentalInterest bug

--- src/Controller/RentalInterestController.php
+++ src/Controller/RentalInterestController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/rental/interest")
+ * @Route("/interest")
  */
 class RentalInterestController extends AbstractController
 {
